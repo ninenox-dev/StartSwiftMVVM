e657be5689a20fadfb420eab8fc41e07138f1b35
Nisit Sirimarnkit <nisit15@hotmail.com>
2019-12-09 10:00:52 +0700

    update

---
+++ StartMVVM/Classes/Login/Model/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  StartMVVM
+//
+//  Created by Nisit Sirimarnkit on 9/12/2562 BE.
+//  Copyright © 2562 Nisit Sirimarnkit. All rights reserved.
+//
+
+import Foundation
+
+class Profile {
+    var user = "nisit"
+    var pass = "12345"
+    init() {
+        
+    }
+
+}
--- StartMVVM/Classes/Login/ViewModel/ViewModel.swift
+++ StartMVVM/Classes/Login/ViewModel/ViewModel.swift
@@ -21,6 +21,11 @@ protocol ViewControllerDelegate: class {
 class ViewModel:ViewModelDelegate {
     
     weak var delegate: ViewControllerDelegate?
+    var user: Profile
+    
+    init() {
+        self.user = Profile()
+    }
     
     func sendValue(from emailTextField: String?, passwordTextField: String?) {
         
@@ -28,7 +33,13 @@ class ViewModel:ViewModelDelegate {
         guard let password = passwordTextField else {return}
         print("email : \(email)")
         print("password : \(password)")
-        delegate?.getInformationBack(handleString: "\(email),\(password)")
+        var result = ""
+        if email == "nisit" && password == "12345" {
+            result = "login success."
+        } else {
+            result = "login fail."
+        }
+        delegate?.getInformationBack(handleString: result)
     }
     
     
